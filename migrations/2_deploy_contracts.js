96c57370bd423ad2df5593f87e2a6a06ac062393
yinhu <>
2021-04-28 17:31:57 +0800

    Migration scripts.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,56 +1,75 @@
-const {ethers} = require("ethers");
-const YAYAToken = artifacts.require('YAYAToken');
-const PAPAToken = artifacts.require('PAPAToken');
-const ItemNFT = artifacts.require('ItemNFT');
-const FishingMaster = artifacts.require('FishingMaster');
-const ItemHelper = artifacts.require('ItemHelper');
+const { ethers } = require("ethers");
+const YAYAToken = artifacts.require("YAYAToken");
+const PAPAToken = artifacts.require("PAPAToken");
+const ItemNFT = artifacts.require("ItemNFT");
+const FishingMaster = artifacts.require("FishingMaster");
+const ItemHelper = artifacts.require("ItemHelper");
 const MockBEP20 = artifacts.require("testlibs/MockBEP20");
 const Multicall = artifacts.require("utils/Multicall");
 
-module.exports = function(deployer) {
+module.exports = function (deployer, network) {
+  deployer.then(async () => {
+    var accounts;
+    await web3.eth.getAccounts(function(err,res) { accounts = res; });
+    var owner = accounts[0]; // first account
 
-    deployer.then(async () => {
-        let devaddr = '0x71E30416eF2D40daC33b649596a0e18E46225576';
-        let initNum = ethers.utils.parseEther("1000");
-        let yayaNum = ethers.utils.parseEther("100000000000");
+    let devaddr = owner;
 
-        let yaya = await deployer.deploy(YAYAToken);
-        await yaya.mint(devaddr,yayaNum);
+    let yayaPerblock = "100";
+    let papaPerblock = "100";
+    let startBlock = "1";
+    let papaCap = "100000000000000000000000000";
 
-        let papa = await deployer.deploy(PAPAToken, '100000000000000000000000000');
-        await papa.mint(devaddr,initNum);
+    let yaya = await deployer.deploy(YAYAToken);
+    let papa = await deployer.deploy(PAPAToken, papaCap);
+    let nft = await deployer.deploy(ItemNFT);
+    let nftHelper = await deployer.deploy(ItemHelper);
 
-        let nft = await deployer.deploy(ItemNFT);
-        let nftHelper = await deployer.deploy(ItemHelper);
-        let master = await deployer.deploy(FishingMaster, yaya.address, papa.address, nft.address, nftHelper.address, devaddr, '40', '40','9');
+    let master = await deployer.deploy(
+      FishingMaster,
+      yaya.address,
+      papa.address,
+      nft.address,
+      nftHelper.address,
+      devaddr,
+      yayaPerblock,
+      papaPerblock,
+      startBlock
+    );
 
-        let lp1 = await MockBEP20.new("LPToken1", "LP1", initNum);
-        let lp2 = await MockBEP20.new("LPToken2", "LP2", initNum);
+    if (network != "bsc") {
+      await yaya.mint(devaddr, ethers.utils.parseEther("100000000000"));
+      await papa.mint(devaddr, ethers.utils.parseEther("1000"));
+    }
 
-        let multicall = await deployer.deploy(Multicall);
+    let multicall = await deployer.deploy(Multicall);
 
-        await master.add("1000", lp1.address, true, true);
-        await master.add("2000", lp2.address, true, true);
+    await yaya.transferOwnership(master.address);
+    await papa.transferOwnership(master.address);
+    await nft.transferOwnership(master.address);
 
-        await yaya.transferOwnership(master.address);
-        await papa.transferOwnership(master.address);
-        await nft.transferOwnership(master.address);
+    if (network != "bsc") {
+      // For test environment
+      await master.mintRandomNFT(devaddr);
+      await master.mintRandomNFT(devaddr);
+      await master.mintRandomNFT(devaddr);
+      await master.mintRandomNFT(devaddr);
+      await master.mintRandomNFT(devaddr);
+      await master.updateMaxLevel(100);
+      let lp1 = await MockBEP20.new("LPToken1", "LP1", ethers.utils.parseEther("1000"));
+      let lp2 = await MockBEP20.new("LPToken2", "LP2", ethers.utils.parseEther("1000"));
 
-        await master.mintRandomNFT(devaddr);
-        await master.mintRandomNFT(devaddr);
-        await master.mintRandomNFT(devaddr);
-        await master.mintRandomNFT(devaddr);
-        await master.mintRandomNFT(devaddr);
-        await master.updateMaxLevel(100);
+      await master.add("1000", lp1.address, true, true);
+      await master.add("2000", lp2.address, true, true);
 
-        console.log("yaya",yaya.address);
-        console.log("papa",papa.address);
-        console.log("nft",nft.address);
-        console.log("nftHelper",nftHelper.address);
-        console.log("master",master.address);
-        console.log("lp1",lp1.address);
-        console.log("lp2",lp2.address);
-        console.log("multicall",multicall.address);
-    });
-    
+      console.log("yaya", yaya.address);
+      console.log("papa", papa.address);
+      console.log("nft", nft.address);
+      console.log("nftHelper", nftHelper.address);
+      console.log("master", master.address);
+      console.log("lp1", lp1.address);
+      console.log("lp2", lp2.address);
+      console.log("multicall", multicall.address);
+    }
+  });
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,10 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const infuraKey = "fj4jll3k.....";
+//
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+
 module.exports = {
   // Uncommenting the defaults below
   // provides for an easier quick-start with Ganache.
@@ -11,11 +18,32 @@ module.exports = {
   //    port: 7545,
   //    network_id: "*",
   //  },
-  ganache: {
+  development: {
     host: "127.0.0.1",     // Localhost (default: none)
-    port: 7545,            // Standard Ethereum port (default: none)
+    port: 8545,            // Standard Ethereum port (default: none)
     network_id: "*",       // Any network (default: none)
   },
+
+  testnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+
+  ganache: {
+        host: "127.0.0.1",     // Localhost (default: none)
+        port: 7545,            // Standard Ethereum port (default: none)
+        network_id: "*",       // Any network (default: none)
+      },
   //  test: {
   //    host: "127.0.0.1",
   //    port: 7545,
